57cced8ef815dabe9cec5a8acf4e019116dd69d7
Ashmi Patel <200388612@student.georgianc.on.ca>
2019-06-23 13:28:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,4 @@ app.get('/', (req, res) => {
 });
 
 
-const server = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+const server = app.listen(app.get('port'));
